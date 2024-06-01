7624032163772330485071e856003285463ef659
mikel-js <michaelcastro.js@gmail.com>
2024-06-01 22:31:09 +0300

    add imports on index file

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,11 +1,17 @@
 import express from 'express';
 import { json } from 'body-parser';
 import { currentUserRouter } from './routes/current-user';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { singinRouter } from './routes/signin';
 
 const app = express();
 app.use(json());
 
 app.use(currentUserRouter);
+app.use(singinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
 app.get('/api/users/currentuser', (req, res) => {
   res.send('hi there');

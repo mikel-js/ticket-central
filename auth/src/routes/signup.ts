353a1b10e4db8cfd9d417af46949ea98a60d60e8
mikel-js <michaelcastro.js@gmail.com>
2024-06-04 22:06:57 +0300

    add database connection error

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 const router = express.Router();
 
@@ -16,11 +18,11 @@ router.post(
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      throw new Error('invalid email or pass');
+      throw new RequestValidationError(errors.array());
     }
 
     console.log('creating user....');
-    throw new Error('Error connecting to DBs');
+    throw new DatabaseConnectionError();
     res.send({});
   }
 );

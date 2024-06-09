8868353dbc78762ad2f4434910a44ca4fcb978a1
mikel-js <michaelcastro.js@gmail.com>
2024-06-09 13:55:04 +0300

    add error message

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 export const errorHandler = (
   err: Error,
@@ -6,8 +8,12 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  console.log('err', err);
-
+  if (err instanceof RequestValidationError) {
+    console.log('handling error');
+  }
+  if (err instanceof DatabaseConnectionError) {
+    console.log('handling db error');
+  }
   res.status(400).send({
     message: err.message,
   });
